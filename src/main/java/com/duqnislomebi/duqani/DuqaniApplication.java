d52ea046d6e4ed689bc536259457996e51136f07
kiu_mikadze.tsotne <mikadze.tsotne@kiu.edu.ge>
2021-05-07 00:32:23 +0400

    Start

---
+++ src/main/java/com/duqnislomebi/duqani/DuqaniApplication.java
@@ -0,0 +1,13 @@
+package com.duqnislomebi.duqani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuqaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuqaniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duqnislomebi/duqani/DuqaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duqnislomebi.duqani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuqaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
