3c752fa42ebdf070690b674009032a57a45ef23c
Beka Modebadze <b.modebadze@ssg.gov.ge>
2021-06-26 20:15:36 +0400

    test commit

--- src/main/java/com/duqnislomebi/duqani/DuqaniApplication.java
+++ src/main/java/com/duqnislomebi/duqani/DuqaniApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DuqaniApplication {
 
+    // test
     public static void main(String[] args) {
         SpringApplication.run(DuqaniApplication.class, args);
     }
