cfad30a86aa6a98ffae350603d0f279336bf88e2
Beka Modebadze <b.modebadze@ssg.gov.ge>
2021-06-28 18:05:30 +0400

    @RequestBody

--- src/main/java/com/duqnislomebi/duqani/controller/FoodFacilityController.java
+++ src/main/java/com/duqnislomebi/duqani/controller/FoodFacilityController.java
@@ -6,6 +6,7 @@ import com.duqnislomebi.duqani.model.SearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class FoodFacilityController {
     }
 
     @GetMapping("getFacilities")
-    public Page<FoodFacility> getFoodFacilities(SearchRequest searchRequest) {
+    public Page<FoodFacility> getFoodFacilities(@RequestBody SearchRequest searchRequest) {
         return foodFacilityFacade.getFoodFacilities(searchRequest);
     }
 }
